cd0de911f50fc9de4ad8f361630a18077ccebeb5
gabriel.compasso <GABRIEL.COMPASSO@fleetcor.com.br>
2022-04-30 19:28:40 -0300

    feat: add eslint, prettier and the configurion files

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'off'
+  }
+};
