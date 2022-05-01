99668813b89759f5a1e4ae6748971fda0dae0a0b
gabriel.compasso <GABRIEL.COMPASSO@fleetcor.com.br>
2022-05-01 16:00:46 -0300

    fix: adjust ResponseHelper from presentation helpers to ensure that the code parameter of expectionHandler method never returns a non-numeric value

--- src/presentation/helpers/response-helper.js
+++ src/presentation/helpers/response-helper.js
@@ -1,9 +1,11 @@
 const { InternalServerError } = require('../../utils/errors');
 
+const isNumeric = (value) => /^-?\d+$/.test(value);
+
 const ok = (data) => ({ data, code: 200 });
 
 const exceptionHandler = (error) => ({
-  code: error.code || 500,
+  code: (isNumeric(error.code) && error.code) || 500,
   data: { error: error.description || new InternalServerError().message }
 });
 
